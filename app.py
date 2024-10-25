57b550736c30930fc45e432f8719a14a7c3acc11
feranmiodugbemi <87916077+feranmiodugbemi@users.noreply.github.com>
2024-10-25 03:13:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -346,4 +346,4 @@ async def delete_article(article_id: str, username: str = Depends(authenticate_u
 
 
 if __name__ == "__main__":
-   uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+   uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
